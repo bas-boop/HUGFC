97d05ffb6e077bb30b85ed675d5134576508cd1e
bas-boop <dereusbas461@gmail.com>
2024-04-27 13:54:10 +0200

    Added player running

--- Assets/Scripts/Player/InputParser.cs
+++ Assets/Scripts/Player/InputParser.cs
@@ -7,6 +7,7 @@ namespace Player
     public sealed class InputParser : MonoBehaviour
     {
         private const string MOVE_ACTION = "Move";
+        private const string RUN_ACTION = "Run";
 
         [SerializeField] private Movement movement;
         
@@ -22,7 +23,12 @@ namespace Player
         private void FixedUpdate()
         {
             Vector2 moveInput = _playerControlsActions[MOVE_ACTION].ReadValue<Vector2>();
-            movement.Walk(moveInput);
+            float runInput = _playerControlsActions[RUN_ACTION].ReadValue<float>();
+            
+            if (moveInput != Vector2.zero)
+                movement.Walk(moveInput, runInput);
+            else
+                movement.SetIdle();
         }
     }
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,6 +2,7 @@
 
 using Framework;
 using Framework.Enums;
+using Framework.Extensions;
 
 namespace Player
 {
@@ -10,7 +11,9 @@ namespace Player
         typeof(InputParser))]
     public sealed class Movement : MonoBehaviour
     {
-        [SerializeField, Range(0.1f, 100)] private float moveSpeed;
+        [SerializeField, Range(0.1f, 50)] private float moveSpeed;
+        [SerializeField, Range(0.2f, 100)] private float runSpeed;
+        [SerializeField] private float deceleration = 2;
         
         private PlayerMoveState _currentState;
         private UniversalGroundChecker _groundChecker;
@@ -22,14 +25,24 @@ namespace Player
             _rigidbody = GetComponent<Rigidbody>();
         }
 
-        public void Walk(Vector2 input)
+        public void Walk(Vector2 moveInput, float runInput)
         {
             if (!_groundChecker.IsGrounded)
                 return;
-            
-            Vector2 direction = input * moveSpeed;
+
+            bool isRunning = runInput > 0;
+            Vector2 direction = moveInput * (isRunning ? runSpeed : moveSpeed);
             Vector3 newVelocity = new (direction.x, _rigidbody.velocity.y, direction.y);
             
+            _rigidbody.velocity = newVelocity;
+            _currentState = runInput > 0 ? PlayerMoveState.RUNNING : PlayerMoveState.WALKING;
+        }
+
+        public void SetIdle()
+        {
+            _currentState = PlayerMoveState.IDLE;
+            Vector3 newVelocity = _rigidbody.velocity;
+            newVelocity.Divide(deceleration,1,deceleration);
             _rigidbody.velocity = newVelocity;
         }
     }
